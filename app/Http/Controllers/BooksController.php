43bb328b7b3083abfebae5c86d47f3465fb01fec
Damiano <damianobertuna@gmail.com>
2021-04-29 23:59:40 +0200

    update .gitignore to exclude .idea directory

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    public function store()
+    {
+        Book::create($this->validateRequest());
+    }
+
+    public function update(Book $book)
+    {
+        $book->update($this->validateRequest());
+    }
+
+    protected function validateRequest(): array
+    {
+        $data = request()->validate([
+            'title' => 'required',
+            'author' => 'required'
+        ]);
+        return $data;
+    }
+}
