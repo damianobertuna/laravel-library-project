bd6a0933e7160bea4304c1c139be5e13f8f61759
Damiano <damianobertuna@gmail.com>
2021-04-30 13:11:11 +0200

    added some new routes for new actions

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,9 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Book extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    public function path()
+    {
+        return '/books/' . $this->id; // . "-" . Str::slug($this->title); if you want to add the title well formatted to the url
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/books', 'App\Http\Controllers\BooksController@store');
 Route::patch('/books/{book}', 'App\Http\Controllers\BooksController@update');
+Route::delete('/books/{book}', 'App\Http\Controllers\BooksController@destroy');
+
+Route::post('/author', 'App\Http\Controllers\AuthorsController@store');
+
+/* with something like slug we can add to the url the title well formatted (in the model we created
+a helper function to return the path in the forma id-title */
+/*Route::patch('/books/{book}-{slug}', 'App\Http\Controllers\BooksController@update');
+Route::delete('/books/{book}-{slug}', 'App\Http\Controllers\BooksController@destroy');*/
