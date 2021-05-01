6eee7d3d153b98410f14442e3cc11e5532dd1246
Damiano <damianobertuna@gmail.com>
2021-05-01 12:12:24 +0200

    added reservation model and added checkin and checkout methods to book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -16,4 +16,39 @@ class Book extends Model
         return '/books/' . $this->id; // . "-" . Str::slug($this->title); if you want to add the title well formatted to the url
     }
 
+    public function checkout($user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+
+    public function checkin($user) {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if (is_null($reservation)) {
+            throw new \Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now()
+        ]);
+    }
+
+    public function setAuthorIdAttribute($author)
+    {
+        $this->attributes['author_id'] = Author::firstOrCreate([
+            'name' => $author,
+        ])->id;
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
+    }
+
 }
